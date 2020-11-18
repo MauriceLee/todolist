db78171c6232e8ae76c9c6def42453fc0dfb52a5
Maurice <maurice@tcastech.com>
2020-11-18 10:02:33 +0800

    v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Title from './components/Title/Title';
+import CreateTodo from './components/CreateTodo/CreateTodo';
+import TodoList from './components/TodoList/TodoList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: ['買牛奶', '繳電話費', '去銀行']
+    }
+  }
+
+  createTodo(newtodo) {
+    this.setState({
+      todos: [...this.state.todos, newtodo]
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <Title todos={this.state.todos}/>
+        <CreateTodo todos={this.state.todos} createTodo={(newtodo) => this.createTodo(newtodo)}/>
+        <TodoList todos={this.state.todos}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/CreateTodo/CreateTodo.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import TodoInput from './TodoInput';
+import CreateTodoButton from './CreateTodoButton';
+
+class CreateTodo extends Component {
+  constructor() {
+    super();
+    this.state = {
+      inputText: '',
+    }
+  }
+
+  createTodo() {
+    if(this.state.inputText) {
+      this.props.createTodo && this.props.createTodo(this.state.inputText);
+      this.setState({
+        inputText: ''
+      })
+    }
+  }
+
+  updateInputText(event) {
+    this.setState({
+      inputText: event.target.value
+    })
+  }
+
+
+  render() {
+    return (
+      <div>
+        <TodoInput inputText={this.state.inputText} updateInputText={(event) => this.updateInputText(event)}/>
+        <CreateTodoButton createTodo={() => this.createTodo()}/>
+        for () {
+
+        }
+      </div>
+    );
+  }
+}
+
+export default CreateTodo
